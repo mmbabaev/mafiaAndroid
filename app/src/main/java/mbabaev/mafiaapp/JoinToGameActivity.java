5276d27e7956b357e3f7011dc95cb80a158371a6
Semen Obukhov <obuhov_s_p@mail.ru>
2016-07-13 16:07:12 +0300

    resolve conflicts

---
+++ app/src/main/java/mbabaev/mafiaapp/JoinToGameActivity.java
@@ -0,0 +1,15 @@
+package mbabaev.mafiaapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class JoinToGameActivity extends AppCompatActivity{
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.join_game);
+    }
+}
